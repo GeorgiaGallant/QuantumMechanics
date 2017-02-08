f4ea8dababf5910b41954aeb20276676c3ac8ccc
Eleanor Seiler <c17es@dalton.org>
2017-02-07 21:39:48 -0500

    code for rosie teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rosie.java
@@ -0,0 +1,130 @@
+/*
+Based on another program by Robert Atkinson. See README for full license.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import android.text.style.TtsSpan;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name="Rosie/", group="Linear Opmode")  // @Autonomous(...) is the other common choice
+public class Rosie extends LinearOpMode {
+
+    /* Declare OpMode members. */
+
+    DcMotor NOM = null;
+    DcMotor FL = null;
+    DcMotor BL = null;
+    DcMotor FR = null;
+    DcMotor BR = null;
+    DcMotor Elevator = null;
+    DcMotor Launch = null;
+    Servo door = null;
+    DcMotor Lift = null;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        FL = hardwareMap.dcMotor.get("FL");
+        BL = hardwareMap.dcMotor.get("BL");
+        FR = hardwareMap.dcMotor.get("FR");
+        BR = hardwareMap.dcMotor.get("BR");
+        NOM = hardwareMap.dcMotor.get("NOM");
+        Elevator = hardwareMap.dcMotor.get("Elevator");
+        Launch = hardwareMap.dcMotor.get("Launch");
+        Lift = hardwareMap.dcMotor.get("Lift");
+        door = hardwareMap.servo.get("door");
+        door.setPosition(.3 );//open
+        // Wait for the game to start (drive,,,,
+                // p-r presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            //driver gamepad 1
+
+            //drive
+            if(Math.abs(gamepad1.left_stick_y) > 0.5) {
+                FL.setPower(-gamepad1.left_stick_y);
+                FR.setPower(gamepad1.left_stick_y);
+                BR.setPower(gamepad1.left_stick_y);
+                BL.setPower(-gamepad1.left_stick_y);
+            }
+            else if(Math.abs(gamepad1.left_stick_x) > 0.5){
+                FL.setPower(-gamepad1.left_stick_x);
+                BL.setPower(-gamepad1.left_stick_x);
+                FR.setPower(-gamepad1.left_stick_x);
+                BR.setPower(-gamepad1.left_stick_x);
+            }else{
+                FL.setPower(0);
+                FR.setPower(0);
+                BR.setPower(0);
+                BL.setPower(0);
+            }
+            //pick up
+
+            if(gamepad1.right_bumper==true){
+                NOM.setPower(-.6);
+                Elevator.setPower(-.6);
+            }
+            else if(gamepad1.left_bumper ==true) {
+                NOM.setPower(.6);
+                Elevator.setPower(.6);
+            }
+            else{
+                NOM.setPower(0);
+                Elevator.setPower(0);
+            }
+            // butto pusher
+
+            //button pusher code goes here!!!!
+
+
+
+
+
+
+
+            //gunner game pad 2
+
+            //launch
+            if(gamepad2.right_trigger > 0.5){
+                Launch.setPower(.6);
+            }
+            else{
+                Launch.setPower(0);
+            }
+
+            //lift
+            if(gamepad2.dpad_up == true){
+                Lift.setPower(.4);
+            }
+            else if(gamepad2.dpad_down == true){
+                Lift.setPower(-.4);
+            }
+            else{
+                Lift.setPower(0);
+            }
+
+            //door
+            if(gamepad2.left_bumper == true){
+               door.setPosition(.85); //open
+            }
+            else{
+                door.setPosition(.3); //closed
+            }
+
+            idle(); // Always call idle() at the bottom of your while(opModeIsActive()) loop
+        }
+    }
+}
+
